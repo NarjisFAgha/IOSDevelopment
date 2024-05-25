23c62e5327c332b8f947d8f47f3e1cf90331169b
NarjisFAgha <nagha6799@conestogac.on.ca>
2024-05-24 22:07:20 -0700

    updated lab3

--- UI_lab3_text/UI_lab3_text/ViewController.swift
+++ UI_lab3_text/UI_lab3_text/ViewController.swift
@@ -14,16 +14,16 @@ class ViewController: UIViewController {
     @IBOutlet weak var lastName_textfield: UITextField!
     @IBOutlet weak var Country_textfield: UITextField!
     @IBOutlet weak var Age_textfield: UITextField!
-    @IBOutlet weak var Output_textfield: UITextField!
+    @IBOutlet weak var Output_textfield: UITextView!
     @IBOutlet weak var Message_Label: UILabel!
   
     
     @IBAction func addInformation_Button(_ sender: UIButton) {
-        self.Output_textfield.text = "Name: \(self.firstName_textfield.text) \(self.lastName_textfield.text) \n Country: (self.Country_textfield.text) \n Age: (self.Age_textfield.text)"
+        self.Output_textfield.text = "Name: \(self.firstName_textfield.text!) \(self.lastName_textfield.text!) \nCountry: \(self.Country_textfield.text!)  \nAge: \(self.Age_textfield.text!)"
     }
     
     @IBAction func Submit_Button(_ sender: UIButton) {
-        if self.firstName_textfield.text == "" && self.lastName_textfield.text == "" && self.Country_textfield.text == "" && self.Age_textfield.text == "" {
+        if self.firstName_textfield.text == "" || self.lastName_textfield.text == "" || self.Country_textfield.text == "" || self.Age_textfield.text == "" {
             self.Message_Label.text = "Please fill all the details!"
         } else{
             self.Message_Label.text = "Successfully submitted!"
