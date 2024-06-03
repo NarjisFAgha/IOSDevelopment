56421c1c806c187dbb58603c423281afcbff97e2
NarjisFAgha <nagha6799@conestogac.on.ca>
2024-06-02 19:18:41 -0700

    zipped

--- week4/UI_lab4/UI_lab4/ViewController.swift
+++ week4/UI_lab4/UI_lab4/ViewController.swift
@@ -10,7 +10,7 @@ import UIKit
 class ViewController: UIViewController {
 
     @IBOutlet weak var Title_Label: UILabel!
-    @IBOutlet weak var Image_stack: UIStackView!
+    @IBOutlet weak var 	Image_stack: UIStackView!
     @IBOutlet weak var Img1: UIImageView!
     @IBOutlet weak var Img2: UIImageView!
     @IBOutlet weak var Img3: UIImageView!
@@ -27,6 +27,43 @@ class ViewController: UIViewController {
     @IBOutlet weak var DOB: UITextField!
     @IBOutlet weak var Output: UITextView!
     
+    func isValidDOB(_ dob: String?) -> Bool {
+           guard let dobText = dob else { return false }
+           
+           let formatter = DateFormatter()
+           formatter.dateFormat = "dd/MM/yyyy"
+           
+           guard let date = formatter.date(from: dobText) else { return false }
+           
+           let age = Calendar.current.dateComponents([.year], from: date, to: Date()).year ?? 0
+           
+           return age > 18
+       }
+       
+       func updateOutput() {
+           guard let firstName = FirstName.text, !firstName.isEmpty,
+                 let surname = SurName.text, !surname.isEmpty,
+                 let address = Address.text, !address.isEmpty,
+                 let city = City.text, !city.isEmpty,
+                 let dob = DOB.text, !dob.isEmpty, isValidDOB(dob) else {
+               Output.text = "Please fill in all fields and ensure the age is over 18."
+               return
+           }
+           
+           let formatter = DateFormatter()
+           formatter.dateFormat = "dd/MM/yyyy"
+           let date = formatter.date(from: dob)!
+           let age = Calendar.current.dateComponents([.year], from: date, to: Date()).year!
+           
+           Output.text = """
+           I, \(firstName) \(surname), currently living at \(address) in the city of \(city) do hereby accept the terms and conditions assignment.
+           I am \(age) and therefore am able to accept the conditions of this assignment.
+           """
+       }
+       
+       
+    
+    
     @IBAction func Reset(_ sender: Any) {
         self.FirstName.text = ""
         self.SurName.text = ""
@@ -42,11 +79,13 @@ class ViewController: UIViewController {
         self.Address.text = ""
         self.City.text = ""
         self.DOB.text = ""
-        self.Output.text = ""
         self.Output.text = "You have Declined."
     }
     
     @IBAction func Accept(_ sender: Any) {
+       
+        updateOutput()
+              
     }
     override func viewDidLoad() {
         super.viewDidLoad()
