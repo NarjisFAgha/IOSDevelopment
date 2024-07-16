a2891267d4691e72d8e49238c7fe2ef2071f1842
NarjisFAgha <nagha6799@conestogac.on.ca>
2024-07-15 20:05:36 -0500

    updated

--- Lab7_GPS/Lab6_GPS/ViewController.swift
+++ Lab7_GPS/Lab6_GPS/ViewController.swift
@@ -14,11 +14,14 @@ class ViewController: UIViewController, CLLocationManagerDelegate{
     @IBOutlet weak var TripStatus: UIView!
     @IBOutlet weak var MapView: MKMapView!
     @IBOutlet weak var SpeedExceedIndicator: UIView!
+  
     @IBOutlet weak var MaxAccelerateDisplay: UILabel!
     @IBOutlet weak var DistCoveredDisplay: UILabel!
     @IBOutlet weak var AvgSpeedDisplay: UILabel!
     @IBOutlet weak var MaxSpeedDisplay: UILabel!
     @IBOutlet weak var CurrentSpeedDisplay: UILabel!
+    
+    @IBOutlet weak var distanceBeforeExceedingDisplay: UILabel!
     
     let locationDetector = CLLocationManager()
     var maxSpd: CLLocationSpeed = 0
@@ -27,6 +30,8 @@ class ViewController: UIViewController, CLLocationManagerDelegate{
     var totalDist: CLLocationDistance = 0
     var lastLoc: CLLocation?
     var maxAcceleration: Double = 0
+    var distanceBeforeExceeding: CLLocationDistance = 0
+    var hasExceededSpeedLimit = false
     
     @IBAction func StartTripButton(_ sender: Any) {
         locationDetector.startUpdatingLocation()
@@ -53,12 +58,14 @@ class ViewController: UIViewController, CLLocationManagerDelegate{
            totalDist = 0
            lastLoc = nil
            maxAcceleration = 0
+        distanceBeforeExceeding = 0
            
         CurrentSpeedDisplay.text = "0 km/h"
         MaxSpeedDisplay.text = "0 km/h"
         AvgSpeedDisplay.text = "0 km/h"
         DistCoveredDisplay.text = "0 km"
         MaxAccelerateDisplay.text = "0 m/s^2"
+        distanceBeforeExceedingDisplay.text = "0 km"
         SpeedExceedIndicator.backgroundColor = .clear
        }
 
@@ -83,12 +90,22 @@ class ViewController: UIViewController, CLLocationManagerDelegate{
                    totalDist += distance
                    DistCoveredDisplay.text = String(format: "%.2f km", totalDist / 1000) // Convert m into km
                    
+                   if !hasExceededSpeedLimit {
+                       distanceBeforeExceeding += distance
+                       distanceBeforeExceedingDisplay.text = String(format: "%.2f km", distanceBeforeExceeding / 1000)
+                       
+                       if ((speed * 3.6) > 115) {
+                           hasExceededSpeedLimit = true
+                       }
+                   }
                    let acceleration = abs(speed - lastLocation.speed) / location.timestamp.timeIntervalSince(lastLocation.timestamp)
                    if acceleration > maxAcceleration {
                        maxAcceleration = acceleration
                        MaxAccelerateDisplay.text = String(format: "%.2f m/s^2", maxAcceleration)
                    }
                }
+      
+                   
                
                lastLoc = location
                
